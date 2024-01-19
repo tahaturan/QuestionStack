164f268908046f564bd306008aa4121c9fa627ca
tahaturan <tahaaturann16@gmail.com>
2024-01-19 14:45:45 +0300

    HomeViewController created a sample UI Desgin and HomeViewTableView cell is created

--- QuestionStack/Scenes/HomeVC/Cell/HomeTableViewCell.swift
+++ QuestionStack/Scenes/HomeVC/Cell/HomeTableViewCell.swift
@@ -6,18 +6,91 @@
 //
 
 import UIKit
+import SnapKit
 
 class HomeTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    //MARK: - Properties
+    static let identifier: String = "homeViewCell"
+    
+    //MARK: - UIComponents
+    private var profileImageView: UIImageView = {
+      let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = .dummy
+        return imageView
+    }()
+    private lazy var titleLabel: UILabel = createLabel(type: .title)
+    private lazy var userNameLabel: UILabel = createLabel(type: .userName)
+    private lazy var tagLabel: UILabel = createLabel(type: .tag)
+    //MARK: - LifeCycle
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
+    
+    private func setupUI() {
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(userNameLabel)
+        contentView.addSubview(tagLabel)
+        
+        profileImageView.snp.makeConstraints { make in
+            make.top.equalTo(contentView.snp.top)
+            make.left.equalTo(contentView.snp.left)
+            make.bottom.equalTo(contentView.snp.bottom)
+            make.width.equalTo(100)
+        }
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(contentView.snp.top)
+            make.left.equalTo(profileImageView.snp.right).offset(10)
+            make.right.equalTo(contentView).offset(-10)
+        }
+        userNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+            make.left.equalTo(titleLabel)
+        }
+        tagLabel.snp.makeConstraints { make in
+            make.top.equalTo(userNameLabel.snp.bottom).offset(10)
+            make.left.right.equalTo(titleLabel)
+            
+        }
+    }
+    
+     func configureCell(question: QuestionItem) {
+        var tagText: String = ""
+        let tags = question.tags
+        for tag in tags {
+            tagText += "#\(tag), "
+        }
+        titleLabel.text = question.title
+        tagLabel.text = tagText
+        userNameLabel.text = question.owner.displayName
+    }
+}
 
+//MARK: - FactoryMethod
+extension HomeTableViewCell {
+   private enum labelType {
+        case title
+        case tag
+        case userName
+    }
+    private func createLabel(type: labelType) -> UILabel {
+        let label: UILabel = UILabel()
+        label.numberOfLines = 2
+        switch type {
+        case .title:
+            label.font = UIFont.systemFont(ofSize: 18, weight: .semibold)
+        case .tag:
+            label.font = UIFont.systemFont(ofSize: 12, weight: .light)
+            label.textColor = .gray
+        case .userName:
+            label.font = UIFont.systemFont(ofSize: 16, weight: .medium)
+        }
+        return label
+    }
 }
--- QuestionStack/Scenes/HomeVC/Views/HomeViewController.swift
+++ QuestionStack/Scenes/HomeVC/Views/HomeViewController.swift
@@ -10,25 +10,89 @@ import SnapKit
 
 class HomeViewController: UIViewController {
     //MARK: - Properties
-    private var homeViewModel: HomeViewModel = HomeViewModel()
+    private lazy var homeViewModel: HomeViewModel = HomeViewModel(service: NetworkManager())
+    private var questionList: [QuestionItem] = []
+    
+    //MARK: - UICompanents
+    let searchBarView: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.placeholder = "Search"
+        return searchBar
+    }()
+    private var tableView: UITableView = UITableView()
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        homeViewModel.loadData()
-        setupLayout()
-        
-        
+        homeViewModel.delegate = self
+        homeViewModel.load()
     }
 }
 //MARK: - Helper
 extension HomeViewController {
     private func setupUI(){
-        view.backgroundColor = .red
+        view.backgroundColor = .white
+        title = "QuestionStack"
+        makeSearcBarView()
+        makeTableView()
     }
-    private func setupLayout(){
-        homeViewModel.questionList.bind { list in
-            //TODO: bind
+}
+//MARK: - makeComponents
+extension HomeViewController{
+    private func makeTableView() {
+        view.addSubview(tableView)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(HomeTableViewCell.self, forCellReuseIdentifier: HomeTableViewCell.identifier)
+        tableView.separatorStyle = .none
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(searchBarView.snp.bottom).offset(20)
+            make.left.right.equalToSuperview().inset(10)
+            make.bottom.equalToSuperview()
+        }
+    }
+    private func makeSearcBarView() {
+        view.addSubview(searchBarView)
+        searchBarView.returnKeyType = .search
+        searchBarView.searchBarStyle = .minimal
+        searchBarView.showsCancelButton = true
+        searchBarView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.left.right.equalToSuperview().inset(10)
+            make.height.equalTo(50)
+        }
+    }
+}
+//MARK: - UItableViewDelagateDataSource
+extension HomeViewController: TableViewDelegateDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return questionList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeTableViewCell.identifier, for: indexPath) as! HomeTableViewCell
+        let question = questionList[indexPath.row]
+        cell.configureCell(question: question)
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
+    }
+    
+}
+
+//MARK: - HomeViewControllerDelagate
+extension HomeViewController: HomeViewModelDelegate {
+    
+    func handleOutput(_ output: HomeViewModelOutput) {
+        DispatchQueue.main.async {
+            switch output {
+            case .questions(let array):
+                self.questionList = array
+            case .error(let networkError):
+                print(networkError.rawValue)
+            }
+            self.tableView.reloadData()
         }
     }
 }
